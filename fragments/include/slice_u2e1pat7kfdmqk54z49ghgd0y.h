e10e8693f1eb31372f916282abd4c21f1569d521
Guenther Brunthaler <gb@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2022-01-28 21:03:07 +0100

    Add support for buffer slices

---
+++ fragments/include/slice_u2e1pat7kfdmqk54z49ghgd0y.h
@@ -0,0 +1,41 @@
+#ifndef HEADER_U2E1PAT7KFDMQK54Z49GHGD0Y_INCLUDED
+#ifdef __cplusplus
+   extern "C" {
+#endif
+
+#include <stddef.h>
+
+/* Describes a writable portion of a memory area. Frequently but not
+ * necessarily dynamically allocated. Can also be a "window" into a larger
+ * object. Primarily meant to represent binary bytes, but also works for
+ * text. */
+typedef struct {
+   char *start; /* Null if the buffer has not been allocated yet. */
+   size_t length; /* Does *not* include the '\0' if it is a C string. */
+   size_t capacity; /* Zero if start == null or if slice is a window. */
+} slice;
+
+/* Describes a read-only portion of a memory area. Frequently but not
+ * necessarily dynamically allocated. Can also be a "window" into a larger
+ * object. Primarily meant to represent binary bytes, but also works for
+ * text. */
+typedef struct {
+   char const *start; /* Null only if length is also 0. */
+   size_t length; /* Does *not* include the '\0' if it is a C string. */
+} slice_view;
+
+/* A buffer slice with zero overhead convertible into a slice_view. */
+typedef union {
+   slice rw; /* The actual buffer. */
+   slice_view ro; /* Read-only view of buffer's current contents. */
+} flexbuf;
+
+/* Converts a writable slice into a read-only slice_view. Only the latter ones
+ * can be used to slice constant string literals (or constant buffer contents
+ * in general). */
+void slice2view_c0(slice_view *dst, slice const *src);
+
+#ifdef __cplusplus
+   }
+#endif
+#endif /* !HEADER_U2E1PAT7KFDMQK54Z49GHGD0Y_INCLUDED */
